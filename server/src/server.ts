3b2344ca754c70d65222548774cab7710498e1b0
urvashidab <urvashi.dabgotra11@gmail.com>
2026-09-20 22:53:56 +0530

    DB connected

---
+++ server/src/server.ts
@@ -0,0 +1,15 @@
+import express from "express";
+// import dotenv from "dotenv";
+
+const app = express();
+
+const port = 5001;
+
+app.get("/", (req, res) => {
+  res.send("Backend is working fine");
+});
+
+// inititalize server
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
